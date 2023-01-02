161ad19c3e35637af0bd1a32b81173f8c89206a3
bluekmky1 <bluekmky@gmail.com>
2023-01-02 15:45:14 +0900

    update ex light switch

--- src/Theme.js
+++ src/Theme.js
@@ -1,13 +1,12 @@
 export const darkTheme = {
   bgColor: "black",
   textColor: "whitesmoke",
-  boxShadow:
-    "rgba(50, 50, 93, 0.25) 0px 30px 60px -12px inset, rgba(0, 0, 0, 0.3) 0px 18px 36px -18px inset",
+  boxShadow: "rgba(0, 0, 0, 0.35) 0px -50px 36px -28px inset",
 };
 
 export const lightTheme = {
   bgColor: "whitesmoke",
   textColor: "black",
   boxShadow:
-    "rgba(0, 0, 0, 0.25) 0px 54px 55px, rgba(0, 0, 0, 0.12) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px, rgba(0, 0, 0, 0.17) 0px 12px 13px, rgba(0, 0, 0, 0.09) 0px -3px 5px",
+    "rgba(0, 0, 0, 0.4) 0px 2px 4px, rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset",
 };
